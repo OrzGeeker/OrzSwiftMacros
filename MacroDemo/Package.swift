73ba405712b74e9880869959b7c99c106af24f41
wangzhizhou <824219521@qq.com>
2024-11-15 20:46:33 +0800

    add cocoapods & cocoapods-spm local dep submodules and DEV mode

--- MacroDemo/Package.swift
+++ MacroDemo/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "MacroDemo",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.macOS(.v14), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
---
+++ README.md
@@ -0,0 +1,11 @@
+# OrzSwiftMacros
+
+swift macro development and use for cocoapods + spm
+
+## Development
+
+```bash
+git clone --recurse-submodules https://github.com/OrzGeeker/OrzSwiftMacros.git 
+cd OrzSwiftMacros/AppDemo
+bundle && bundle exec pod install && xed .
+```
