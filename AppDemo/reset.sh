ee5ba57bef9b253dd5652603f3fdc11ce5821ebc
wangzhizhou <824219521@qq.com>
2024-11-28 02:46:25 +0800

    adjust

--- AppDemo/reset.sh
+++ AppDemo/reset.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #-*- coding: utf-8 -*-
-
+unset DEV
 bundle update && bundle exec pod update && xed .
--- AppDemo/setup.sh
+++ AppDemo/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #-*- coding: utf-8 -*-
-
+unset DEV
 bundle install && bundle exec pod install && xed .
